6a4c0077dd51bc54afe40624f58d522807f3fc27
Akhil-ps-dev <psakhil211@gmail.com>
2022-10-04 15:24:15 +0530

    frondent-completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_flutter_begins/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +9,56 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        primarySwatch: Colors.blue,
+    //!first
+    //! tell our materialapp that we are using bloc
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-  
-  
-
-  
-  
-  
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      
-      
-      
-      
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
     return Scaffold(
-      appBar: AppBar(
-        
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        
-        child: Column(
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+          child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //!Adding incriment and decriment
+            FloatingActionButton(
+              onPressed: () {
+                //!two method of calling
+                // BlocProvider.of<CounterBloc>(context).add(Incriment());
+
+                context.read<CounterBloc>().add(Incriment());
+              },
+              child: Icon(Icons.add),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            BlocBuilder<CounterBloc, CounterState>(
+              builder: (context, state) => Text('${state.count}'),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                BlocProvider.of<CounterBloc>(context).add(Decriment());
+              },
+              child: Icon(Icons.minimize),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      )),
     );
   }
 }
